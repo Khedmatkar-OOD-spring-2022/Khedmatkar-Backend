a1e2024accccbb7ed0486e91dd699ee974be3e36
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2022-06-28 17:16:37 +0430

    disable cors.

--- src/main/java/com/khedmatkar/demo/config/SecurityConfig.java
+++ src/main/java/com/khedmatkar/demo/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,7 +31,8 @@ public class SecurityConfig {
                 .logout()
                     .and()
                 .csrf()
-                    .disable();
+                    .disable()
+                .cors(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
