418ce5c4a694b0429e38dc35dedec5cc29b6dd6e
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2022-06-30 15:19:31 +0430

    Handlers for successful and unsuccessful login.

--- src/main/java/com/khedmatkar/demo/config/SecurityConfig.java
+++ src/main/java/com/khedmatkar/demo/config/SecurityConfig.java
@@ -7,11 +7,26 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -28,7 +43,14 @@ public class SecurityConfig {
         http
                 .formLogin()
                     .usernameParameter("email")
-                    .and()
+                    .successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                            //do nothing
+                        }
+                    })
+                    .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .and()
                 .logout()
                     .and()
                 .csrf()
