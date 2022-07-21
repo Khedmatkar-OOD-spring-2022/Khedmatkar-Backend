15d0d466150af788721b69d52535aed665a58d70
mohammadrezaabdi <reza_abdi20@yahoo.com>
2022-07-21 12:19:11 +0430

    add api docs and swagger UI

--- src/main/java/com/khedmatkar/demo/DemoApplication.java
+++ src/main/java/com/khedmatkar/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.khedmatkar.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "khedmatkar-api", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(info = @Info(title = "Khedmatkar API"))
 public class DemoApplication {
 
     public static void main(String[] args) {
