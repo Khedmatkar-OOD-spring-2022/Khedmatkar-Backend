4a0d6150cd4eab967f5b8b94d025ce1af195f7ac
mohammadrezaabdi <wnick2085@gmail.com>
2022-08-23 23:37:14 +0430

    add exception message to login request

--- src/main/java/com/khedmatkar/demo/security/SecurityConfig.java
+++ src/main/java/com/khedmatkar/demo/security/SecurityConfig.java
@@ -1,27 +1,24 @@
 package com.khedmatkar.demo.security;
 
-import lombok.extern.log4j.Log4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
-import java.io.IOException;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -37,21 +34,36 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .formLogin()
-                    .usernameParameter("email")
-                    .successHandler(new AuthenticationSuccessHandler() {
-                        @Override
-                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                            //do nothing
-                        }
-                    })
-                    .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .usernameParameter("email")
+                .successHandler((request, response, authentication) -> {
+                    //do nothing
+                })
+                .failureHandler((request, response, exception) -> {
+                    HttpStatus httpStatus = HttpStatus.FORBIDDEN; // 403
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("timestamp", new Date());
+                    data.put("code", httpStatus.value());
+                    data.put("status", httpStatus.name());
+                    data.put("message", "username or password is incorrect.");
+
+                    // setting the response HTTP status code
+                    response.setStatus(httpStatus.value());
+                    // Jackson JSON serializer instance
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    // serializing the response body in JSON
+                    response
+                            .getOutputStream()
+                            .println(
+                                    objectMapper.writeValueAsString(data)
+                            );
+                })
                 .and()
                 .logout()
-                    .and()
+                .and()
                 .csrf()
-                    .disable()
+                .disable()
                 .cors()
-                    .configurationSource(corsConfigurationSource());
+                .configurationSource(corsConfigurationSource());
         return http.build();
     }
 
@@ -69,7 +81,6 @@ public class SecurityConfig {
 
 
 @Component
-@Log4j
 class SessionListener implements HttpSessionListener {
 
     @Override
